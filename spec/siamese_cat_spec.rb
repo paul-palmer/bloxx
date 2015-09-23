fb055bd05b1dbd3f3c6171d44cc46227c386ff86
Paul Palmer <paul@ionic.com>
2015-09-23 16:16:49 -0400

    wolf test cases

---
+++ spec/siamese_cat_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+describe SiameseCat do
+
+  it 'can be created' do
+    cat = SiameseCat.new
+
+    expect(SummonCommand.new(cat).to_s).to eq %q<summon Wolf {}>
+  end
+
+  it 'with complex attributes' do
+    duration = 999999
+
+    cat = SiameseCat.new
+    cat.name = %<Whiskers>
+    cat.always_show_name
+    cat.owner_uuid = '4af775e8-2d26-45b9-a824-89c91110fb79'
+    cat.persistant
+    cat.silent
+    cat.healrate = 40
+    cat.attrs   << MaxHealth.new(80)
+    cat.effects << Regeneration.new(6, duration, false)
+    cat.effects << Resistance.new(4, duration, false)
+    cat.effects << FireResist.new(1-1, duration, false)
+    cat.effects << WaterBreath.new(1-1, duration, false)
+
+    expect(SummonCommand.new(cat).to_s).to eq %q<summon Wolf {CustomName:RazorTooth,CustomNameVisible:1,OwnerUUID:4af775e8-2d26-45b9-a824-89c91110fb79,CollarColor:4,PersistenceRequired:1,Silent:1,AbsorptionAmount:40,ActiveEffects:[{Id:10,Amplifier:6,Duration:999999,ShowParticles:0},{Id:11,Amplifier:4,Duration:999999,ShowParticles:0},{Id:8,Amplifier:2,Duration:999999,ShowParticles:0},{Id:12,Amplifier:0,Duration:999999,ShowParticles:0},{Id:13,Amplifier:0,Duration:999999,ShowParticles:0}],Attributes:[{Name:generic.attackDamage,Base:15},{Name:generic.followRange,Base:4},{Name:generic.knockbackResistance,Base:0},{Name:generic.maxHealth,Base:80}]}>
+  end
+
+end
